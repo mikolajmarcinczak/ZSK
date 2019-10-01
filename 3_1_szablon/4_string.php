3cca760b43b72a16b2685d3d41c887dcd68fc1c7
mikolajmarcinczak <mikolajmarcinczak@users.noreply.github.com>
2019-10-01 10:27:29 +0200

    stringsd

---
+++ 3_1_szablon/4_string.php
@@ -0,0 +1,26 @@
+﻿<?php
+    $text = <<< TEKST
+        zsk - Zespół
+        Szkół
+        Komunikacji
+    TEKST;
+
+    echo "Przed wywołaniem funkcji nl2br: <br> $text";
+    echo "<br>Po wywołaniem funkcji nl2br: <br>";
+    echo nl2br($text),'<br>';
+
+    $test = 'jANuSz';
+
+    echo strtolower($test), '<br>';
+    echo strtoupper($test), '<br>';
+
+    $test = 'janUsz Tomasz agata';
+
+    echo ucfirst($test), '<br>';
+
+    echo ucwords($test), '<br>';
+
+    ##############################################################
+
+
+?>
--- 3_1_szablon/index.php
+++ 3_1_szablon/index.php
@@ -1,5 +1,5 @@
 ﻿<?php
-    require_once('./1.php')
-    require_once('./2.php')
-    require_once('./3.php')
+    require_once('./1.php');
+    require_once('./2.php');
+    require_once('./3.php');
 ?>
